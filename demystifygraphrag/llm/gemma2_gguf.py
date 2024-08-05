79ade558f8cdf60bfbfe58e61ac75674c15b94dd
benste <benstmrs@gmail.com>
2024-08-05 14:51:26 +0200

    bugfix in chatnames

--- demystifygraphrag/llm/gemma2_gguf.py
+++ demystifygraphrag/llm/gemma2_gguf.py
@@ -10,20 +10,22 @@ class Gemma2GGUF(LLM):
     def __init__(self, model_path: str, tokenizer_URI: str, context_size: int = 8192):
         self.model = Llama(model_path=model_path, verbose=False, n_ctx=context_size)
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_URI)
-        self.chatnames = ChatNames(user = 'user', model = 'tokenizer')
+        self.chatnames = ChatNames(user = 'user', model = 'assistant')
 
     def run_chat(self, chat: List[dict], stream: bool = False) -> str:
         llm_input = self.tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True)
         results = self.model(llm_input, max_tokens=-1, stop=["<eos>"], echo=False, stream=stream)
         if stream:
             results = self.print_streamed(results)
+        else:
+            results = results['choices'][0]['text']
             
         return results
         
     def tokenize(self, content: str) -> List[str]:
         return self.tokenizer.tokenize(content)
     
-    def untokenize(self, tokens: List[str]):
+    def untokenize(self, tokens: List[str]) -> str:
         return self.tokenizer.convert_tokens_to_string(tokens)
     
 
